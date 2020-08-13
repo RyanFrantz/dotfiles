05c1f64b5d61e83761ed6a5da6aeefc6027ba9c0
Ryan Frantz <ryanleefrantz@gmail.com>
2020-08-13 16:19:31 -0400

    Properly quote variable.
    - Without this quoting, we can't correctly test the length of the
    variable's value (both -n and -z tests return true).
    - Cleans up dead code/comment.

--- home/.config/fish/functions/ssh_agent_is_running.fish
+++ home/.config/fish/functions/ssh_agent_is_running.fish
@@ -1,9 +1,8 @@
 # Test if an ssh-agent process is defined and running.
 function ssh_agent_is_running
-    if test -n $SSH_AGENT_PID
+    if test -n "$SSH_AGENT_PID"
         # Suppress the header from `ps`.
 		set proc_name (ps -p $SSH_AGENT_PID -o comm='')
-        #if test $proc_name = "ssh-agent"
         if test "$proc_name" = "ssh-agent"
             return
         else
