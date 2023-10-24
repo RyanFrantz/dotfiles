43d6e3ad8bd79535b1469e3df33d87eb3c5b32a3
Ryan Frantz <ryanleefrantz@gmail.com>
2023-10-24 09:53:06 -0400

    Everyday improvements.

--- home/.config/nvim/after/plugin/autoclose.lua
+++ home/.config/nvim/after/plugin/autoclose.lua
@@ -1,1 +1,7 @@
-require("autoclose").setup()
+-- It's rare that I need a closing single quote and it's annoying to remove
+-- them from my comments when I use contractions.
+require("autoclose").setup({
+  keys = {
+    ["'"] = { close = false }
+  }
+})
