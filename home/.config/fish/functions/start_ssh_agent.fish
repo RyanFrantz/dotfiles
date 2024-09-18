507a3e143f09d5b8c660b628f3f7f3eb09ad0b97
Ryan Frantz <ryan.frantz@sscinc.com>
2024-09-18 16:57:23 -0400

    Remove text coloring.
    - Somehow (I don't know at the moment) the colors defined by `set_color`
    leak into the `ssh-agent` or `ssh` process. When attempting to tunnel
    through an SSH jump host, weird debug messages show up like:
      `debug1: kex_exchange_identification: banner line 4: \033[m\017SSH-2.0-OpenSSH_8.0`
    - That error is misleading because that string does not come from a banner (i.e.
    the `Banner` directive); it's the version string that an SSH server returns
    before authentication even begins.
    - I'd love to learn more about how this interaction occurred, but I'm
      behind on my work.

--- home/.config/fish/functions/start_ssh_agent.fish
+++ home/.config/fish/functions/start_ssh_agent.fish
@@ -3,7 +3,7 @@ function start_ssh_agent
     if test -f "$SSH_ENV_FISH"
         source $SSH_ENV_FISH
     else
-        set_color yellow; echo "No agent environment information found!"; set_color normal
+        echo "No agent environment information found!"
         _start_ssh_agent
         return
     end
@@ -12,17 +12,13 @@ function start_ssh_agent
     # Test for agent forwarding. If we're forwarding, running/using a local agent
     # will interfere.
     if ssh_agent_is_forwarding
-		set_color yellow
         echo "SSH agent forwarding detected. I will not start ssh-agent."
-		set_color normal
         return
     end
 
     echo -e "Testing for an existing ssh-agent..."
     if ssh_agent_is_running
-		set_color yellow
         echo "ssh-agent is already running!"
-		set_color normal
         # Attempt to source the relevant environment variables that will allow
         # us to use the existing agent.
         source $SSH_ENV_FISH > /dev/null # source the output to set some env vars
@@ -31,10 +27,10 @@ function start_ssh_agent
         set result $status
         # 0 == we have keys; 1 == we can talk to the agent but no keys are loaded.
         if test $result -eq 0 -o $result -eq 1
-            set_color green; echo "Successfully confirmed ssh-agent is accessible."; set_color normal
-			return
+            echo "Successfully confirmed ssh-agent is accessible."l
+      return
         else
-            set_color red; echo "Failed to connect to ssh-agent!"; set_color normal
+            echo "Failed to connect to ssh-agent!"
             return 1
         end
     else
