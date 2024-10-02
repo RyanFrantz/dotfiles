5a21d43c5ca10acfaa7e72d1861f753bf11d0aeb
Ryan Frantz <ryanleefrantz@gmail.com>
2024-10-02 14:50:15 -0400

    Clean up trailing semicolon

--- home/.config/fish/functions/_start_ssh_agent.fish
+++ home/.config/fish/functions/_start_ssh_agent.fish
@@ -1,7 +1,7 @@
 function _start_ssh_agent
         echo "Initializing ssh-agent..."
         # Ensure these variables have universal scope.
-        ssh-agent -c | sed 's/^setenv/set -Ux/' | sed 's/^echo/#echo/' > $SSH_ENV_FISH
+        ssh-agent -c | sed 's/^setenv/set -Ux/' | sed 's/^echo/#echo/' | sed 's/;$//' > $SSH_ENV_FISH
         chmod 600 $SSH_ENV_FISH
         source $SSH_ENV_FISH > /dev/null # source the output to set some env vars
 end
